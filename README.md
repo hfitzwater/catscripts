b148faca1d6f6031ccb50e3b3fd6a63a8f02bdc7
Hunter Fitzwater <hcfitzwater@gmail.com>
2021-04-07 18:46:11 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# What is this?
+cat scripts property of the package.json in the current directory
---
+++ index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const packageLocation = `${__dirname}/package.json`;
+const packageExists = fs.existsSync(packageLocation);
+
+if( packageExists ) {
+  try {
+    const rawContents = fs.readFileSync(packageLocation);
+    const rawStr = rawContents.toString('utf8');
+    const jsonContents = JSON.parse(rawStr);
+
+    const scripts = Object.keys(jsonContents.scripts).map(key => {
+      return `${key}: ${jsonContents.scripts[key]}`;
+    });
+
+    const scriptStrs = scripts.join('\n');
+
+    process.stdout.write(scriptStrs);
+  } catch( ex ) {
+    process.stdout.write('Something went wrong parsing package.json');
+  }
+} else {
+  process.stdout.write('There is no package.json in this directory');
+}

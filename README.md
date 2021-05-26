5ba5723c1f97b50fb819395f7af5c759e30a7b80
Hunter Fitzwater <hunter.fitzwater@jobscan.co>
2021-05-26 18:44:33 -0400

    add interactive mode

--- README.md
+++ README.md
@@ -1,15 +1,26 @@
 # What is this?
+
 cat scripts property of the package.json in the current directory
 
 # Install
+
 $ npm install -g catscripts
 
 # Run
+
 $ catscripts
 
 # Or
 
 $ npx catscripts
+
+# Options
+
+--help Show help [boolean]
+
+--version Show version number [boolean]
+
+-i, --interactive Run interactively and select a script [boolean]
 
 # Credits
 
--- index.js
+++ index.js
@@ -1,25 +1,87 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
-const chalk = require('chalk');
+const fs = require("fs");
+const { spawn } = require("child_process");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const argv = require("yargs/yargs")(process.argv.slice(2)).option(
+  "interactive",
+  {
+    alias: "i",
+    type: "boolean",
+    description: "Run interactively and select a script",
+  }
+).argv;
 
-const packageLocation = `${process.cwd()}/package.json`;
-const packageExists = fs.existsSync(packageLocation);
+catscripts(argv.i);
 
-if( packageExists ) {
+function catscripts(interactive = false) {
+  const lockFiles = {
+    npm: `${process.cwd()}/package-lock.json`,
+    yarn: `${process.cwd()}/yarn.lock`,
+  };
+  const packageLocation = `${process.cwd()}/package.json`;
+  const packageExists = fs.existsSync(packageLocation);
+  const isYarn = fs.existsSync(lockFiles.yarn);
+
+  if (!packageExists) {
+    process.stdout.write("There is no package.json in this directory");
+  }
+
   try {
     const rawContents = fs.readFileSync(packageLocation);
-    const rawStr = rawContents.toString('utf8');
+    const rawStr = rawContents.toString("utf8");
     const jsonContents = JSON.parse(rawStr);
 
-    Object.keys(jsonContents.scripts).forEach(key => {
-      process.stdout.write(chalk.green(key));
-      process.stdout.write(': ' + jsonContents.scripts[key] + '\n');
+    const choices = Object.keys(jsonContents.scripts).map((key) => {
+      return `${chalk.green(key)}: ${jsonContents.scripts[key]}`;
     });
-  } catch( ex ) {
-    process.stdout.write('Something went wrong parsing package.json');
+
+    if (!interactive) {
+      choices.forEach((c) => {
+        process.stdout.write(c + "\n");
+      });
+    } else {
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "scripts",
+            message: `${isYarn ? "yarn" : "npm"} run `,
+            choices,
+          },
+        ])
+        .then((answer) => {
+          const index = choices.indexOf(answer.scripts);
+          const script = Object.keys(jsonContents.scripts)[index];
+
+          run(isYarn ? "yarn" : "npm", ["run", script]);
+        })
+        .catch((error) => {
+          if (error.isTtyError) {
+            return;
+          }
+
+          process.stdout.write(error);
+        });
+    }
+  } catch (ex) {
+    process.stdout.write("Sorry. Something went wrong.");
   }
-} else {
-  process.stdout.write('There is no package.json in this directory');
+}
+
+function run(cmd, options) {
+  const task = spawn(cmd, options);
+
+  task.stdout.on("data", (data) => {
+    process.stdout.write(`${data}`);
+  });
+
+  task.stderr.on("data", (data) => {
+    process.stderr.write(`${data}`);
+  });
+
+  task.on("error", (error) => {
+    process.stderr.write(`${error.message}`);
+  });
 }
